f7ff0288b3d35969c8603edbb099ed00089fa184
danial akbari <danial_akbari33@hotmail.com>
2018-11-28 12:40:40 +0330

    connectionPool

--- core/sql/index.js
+++ core/sql/index.js
@@ -3,23 +3,28 @@ const mssql = require('mssql');
 
 module.exports = {
     query: async function (command, ...params) {
-        try {
-            let pool = await mssql.connect(sqlConfig);
-            let request = pool.request();
-            for (let index in params) {
-                request = request.input('P' + index, mssql.NVarChar(8000), params[index])
+
+            const pool = new mssql.ConnectionPool(sqlConfig);
+            pool.on('erro', err=>{console.error('sql error', err)});
+            try{
+                await pool.connect();
+                let request = pool.request();
+                for (let index in params) {
+                    request = request.input('P' + index, mssql.NVarChar(8000), params[index])
+                }
+                let result1 = await request.query(command);
+                //res.send(JSON.parse(result1.recordset[0].Structure));
+                return result1.recordset;
             }
-            let result1 = await request.query(command);
+            catch (e) {
+                console.error('sql error' + e)
+            }
+            finally {
+                pool.close();
 
-            return result1.recordset;
-        } catch (err) {
-            console.error(err);
-        } finally {
-            mssql.close();
+            }
         }
-
-
-    },
+    ,
     exec: async function (command, ...params) {
             // Stored procedure
             const pool = new mssql.ConnectionPool(sqlConfig);
